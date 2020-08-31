4a036528c75f73caa199c574e4b64a1cd1b8f72e
IvanMaximenko2004 <69015616+IvanMaximenko2004@users.noreply.github.com>
2020-08-31 23:33:22 +0300

    Check my branch (#2)
    * Пробный коммит
    
    * Пробный коммит
    
    * Пробный коммит
    
    * Пробный коммит
    
    * Ну, мальчика я сделал, дай наверное правда что-то на перёд а то я до пятницы со скуки сдохну)
    
    * Вот, это мы по практике прошли, + теория и разобрались с гитом

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -2,10 +2,13 @@ package com.company.task2;
 
 import com.company.task2.Human;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        Human human = new Human("Dima", 13, 1.75, 65.8);
-        human.say();
+        Human girl1 = new Girl("Nastya", 19, 1.79, 60.6);
+        Human boy1 = new Boy("Dima", 20, 1.90, 85.4);
+        girl1.say();
+        boy1.say();
     }
 }
