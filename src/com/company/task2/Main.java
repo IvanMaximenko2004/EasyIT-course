eab14b2e10444c912bf4fa6525bca70091adc7c9
IvanMaximenko <69015616+IvanMaximenko2004@users.noreply.github.com>
2020-08-31 20:18:41 +0300

    Пробный коммит

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -5,7 +5,7 @@ import com.company.task2.Human;
 public class Main {
 
     public static void main(String[] args) {
-        Human human = new Human("Dima", 14, 1.75, 65.8);
-        human.say();
+        Human man = new Human("Dima", 14, 1.75, 65.8);
+        man.say();
     }
 }
