d022584de7bd759947f727de57e63ec54a32450d
IvanMaximenko <69015616+IvanMaximenko2004@users.noreply.github.com>
2020-08-31 23:19:50 +0300

    Ну, мальчика я сделал, дай наверное правда что-то на перёд а то я до пятницы со скуки сдохну)

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -5,7 +5,9 @@ import com.company.task2.Human;
 public class Main {
 
     public static void main(String[] args) {
-        Human man = new Human("Dima", 14, 1.75, 65.8);
-        man.say();
+        Human girl1 = new Girl("Nastya", 19, 1.79, 60.6);
+        Human boy1 = new Boy("Dima", 20, 1.90, 85.4);
+        girl1.say();
+        boy1.say();
     }
 }
