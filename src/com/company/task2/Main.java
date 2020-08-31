d8a77b2d94ca3f2f0aea9f5939fdf58e79393e4d
IvanMaximenko <69015616+IvanMaximenko2004@users.noreply.github.com>
2020-08-31 23:50:05 +0300

    .

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -6,9 +6,9 @@ import com.company.task2.Human;
 public class Main {
 
     public static void main(String[] args) {
-        Human girl1 = new Girl("Nastya", 19, 1.79, 60.6);
+        Human girl = new Girl("Nastya", 19, 1.79, 60.6);
         Human boy1 = new Boy("Dima", 20, 1.90, 85.4);
-        girl1.say();
+        girl.say();
         boy1.say();
     }
 }
