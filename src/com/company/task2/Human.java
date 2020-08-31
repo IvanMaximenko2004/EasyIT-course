da71e5a0369a8d0b933167a8233a60fbce6f5168
IvanMaximenko <69015616+IvanMaximenko2004@users.noreply.github.com>
2020-08-31 19:59:22 +0300

    FirstComiit

---
+++ src/com/company/task2/Human.java
@@ -0,0 +1,19 @@
+package com.company.task2;
+
+public class Human {
+    public String name;
+    public int age;
+    public double height;
+    public double weight;
+
+    public Human(String name, int age, double height, double weight){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public void say(){
+        System.out.println("My name is " + name + ", my age is " + age + "y, my height is " + height + "m, my weight is " + weight + "kg.");
+    }
+}
