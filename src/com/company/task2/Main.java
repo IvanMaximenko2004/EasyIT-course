3b31c295a77079b0796c69a225f4aa12c4e40723
IvanMaximenko <69015616+IvanMaximenko2004@users.noreply.github.com>
2020-09-05 17:03:10 +0300

    Ну можно так

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -8,9 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Friend friend = new Friend("Dima", 17, 1.84, 80, new Hobby("Volleyball", 5, "Intr game"));
-        Girl girl = new Girl("Nastya", 19, 1.79, 60.6, new Hobby("Football", 2, "The game, where players kick the ball"), friend);
-        Boy boy = new Boy("Sergei", 18, 1.80, 78.3, new Hobby("Tennis", 3, "Enjoy playing tennis"), friend);
+        Girl girl = new Girl("Nastya", 19, 1.79, 60.6,
+                new Hobby("Football", 2, "The game, where players kick the ball"),
+                new Friend("Denis", 17, 1,75,
+                        new Hobby("Volley", 4, "Good game")));
+        Boy boy = new Boy("Sergei", 18, 1.80, 78.3,
+                new Hobby("Tennis", 3, "Enjoy playing tennis"),
+                new Friend("Daniil", 19, 1.83, 80,
+                        new Hobby("Football", 3, "The game, where players kick the ball")));
         girl.getFriend().tell();
+        boy.getFriend().tell();
     }
 }
